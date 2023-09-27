dc4595f55c81daa9dbd34b2bd27f7257345c405b
JayanDesilva <jaycdesilva@gmail.com>
2023-09-27 03:22:24 -0400

    Spring initializer project

---
+++ student_registry/src/test/java/com/gitrepos/studentregistry/student_registry/StudentRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitrepos.studentregistry.student_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
